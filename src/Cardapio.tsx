12f59092f61c603e670e42ea384753994cd97844
gabigpalharini <gabigplharini@gmail.com>
2024-03-21 14:29:34 -0300

    modified:   src/Cardapio.tsx 	new file:   src/assets/image/refri.png 	new file:   src/assets/image/whats.png

--- src/Cardapio.tsx
+++ src/Cardapio.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, Image, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View, YellowBox } from "react-native";
+import { FlatList, Image, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View, YellowBox, Linking } from "react-native";
 
 interface CardapioItem {
     id: string;
@@ -7,25 +7,86 @@ interface CardapioItem {
     preço: string;
     ingredientes: string;
     image: any,
+    adicionar:string,
 }
+
+ 
 const dados: CardapioItem[] = [
-    { id: "1", nome: "X-burguer", preço: "R$17,00", ingredientes: "pão, hamburguer, mussarela e presunto", image: require('./assets/image/xburguer.png')},
-    { id: "2", nome: "X-calabresa", preço: "R$22,00", ingredientes: "pão, hamburguer, mussarela, presunto e calabresa", image:require('./assets/image/xcalabresa.png') },
-    { id: "3", nome: "X-ovo", preço: "R$17,00", ingredientes: "pão, hamburguer, mussarela, presunto e ovo", image: require('./assets/image/xovo.png') },
-    { id: "4", nome: "X-bacon", preço: "R$26,00", ingredientes: "pão, hamburguer, mussarela,  presunto e bacon", image:require('./assets/image/xbacon.png') },
-    { id: "5", nome: "X-frango", preço: "R$25,00", ingredientes: "pão, frango empanado, mussarela e presunto", image: require('./assets/image/xfrango.png') },
-    { id: "6", nome: "X-salada", preço: "R$20,00", ingredientes: "pão, frango empanado, mussarela, presunto, alface, tomate, cebola roxa e maionese ", image: require('./assets/image/xsalada.png') },
-    { id: "7", nome: "X-milho", preço: "R$17,00", ingredientes: "pão, hamburguer, mussarela, presunto e creme de milho", image: require('./assets/image/xmilho.png') },
-    { id: "8", nome: "X-tudo", preço: "R$30,00", ingredientes: "pão, hamburguer, mussarela, presunto, salada, bacon,calabresa e batata palha", image: require('./assets/image/xtudo.png') },
-    { id: "9", nome: "Batata-frita", preço: "R$18,00", ingredientes: "batata frita", image: require('./assets/image/batata.png') },
-    { id: "10", nome: "Batata-frita com cheddar e bacon", preço: "R$27,00", ingredientes: "batata-frita com fatias de bacon e cheddar cremoso", image: require('./assets/image/batatachedarebacon.png') },
-    { id: "11", nome: "Nuggets", preço: "R$17,00", ingredientes: "pedaços de frango frito", image:  require('./assets/image/nuggets.png') },
-    { id: "12", nome: "Tirinhas de frango", preço: "R$25,00", ingredientes: "frango empanado com acompanhamento de molhos", image:  require('./assets/image/frangoempanado.png')  },
-    { id: "13", nome: "Cebola frita", preço: "R$15,00", ingredientes: "cebolas fritas em rodelas", image:  require('./assets/image/cebolafrita.png') },
-    { id: "14", nome: "Milkshake de chocolate", preço: "R$10,00", ingredientes: "leite,sorvete de chocolate e calda de chocolate ", image:  require('./assets/image/milkshakechoco.png') },
-    { id: "15", nome: "Milkshake de morango", preço: "R$13,00", ingredientes: "leite,sorvete de morango, calda de morango e a fruta ", image:  require('./assets/image/milkshakemorango.png') },
+    { id: "1", nome: "X-burguer", preço: "R$17,00", ingredientes: "pão, hamburguer, mussarela e presunto", image: require('./assets/image/xburguer.png'),adicionar:"adicionar +"},
+    { id: "2", nome: "X-calabresa", preço: "R$22,00", ingredientes: "pão, hamburguer, mussarela, presunto e calabresa", image:require('./assets/image/xcalabresa.png'),adicionar:"adicionar +" },
+    { id: "3", nome: "X-ovo", preço: "R$17,00", ingredientes: "pão, hamburguer, mussarela, presunto e ovo", image: require('./assets/image/xovo.png') ,adicionar:"adicionar +"},
+    { id: "4", nome: "X-bacon", preço: "R$26,00", ingredientes: "pão, hamburguer, mussarela,  presunto e bacon", image:require('./assets/image/xbacon.png'),adicionar:"adicionar +" },
+    { id: "5", nome: "X-frango", preço: "R$25,00", ingredientes: "pão, frango empanado, mussarela e presunto", image: require('./assets/image/xfrango.png'),adicionar:"adicionar +" },
+    { id: "6", nome: "X-salada", preço: "R$20,00", ingredientes: "pão, frango empanado, mussarela, presunto, alface, tomate, cebola roxa e maionese ", image: require('./assets/image/xsalada.png'),adicionar:"adicionar +" },
+    { id: "7", nome: "X-milho", preço: "R$17,00", ingredientes: "pão, hamburguer, mussarela, presunto e creme de milho", image: require('./assets/image/xmilho.png') ,adicionar:"adicionar +"},
+    { id: "8", nome: "X-tudo", preço: "R$30,00", ingredientes: "pão, hamburguer, mussarela, presunto, salada, bacon,calabresa e batata palha", image: require('./assets/image/xtudo.png') ,adicionar:"adicionar +"},
+    
+   
+   
+   
    
 ];
+interface acompanhamentos {
+    id: string;
+    nome: string;
+    preço: string;
+    ingredientes: string;
+    image: any,
+    adicionar:string,
+}
+
+
+
+const dados3: acompanhamentos[] = [
+    { id: "9", nome: "Batata-frita", preço: "R$18,00", ingredientes: "batata frita", image: require('./assets/image/batata.png'),adicionar:"adicionar +" 
+    },
+    { id: "10", nome: "Batata-frita com cheddar e bacon", preço: "R$27,00", ingredientes: "batata-frita com fatias de bacon e cheddar cremoso", image: require('./assets/image/batatachedarebacon.png') ,adicionar:"adicionar +" 
+    },
+    { id: "11", nome: "Nuggets", preço: "R$17,00", ingredientes: "pedaços de frango frito", image:  require('./assets/image/nuggets.png') ,adicionar:"adicionar +"
+    },
+    { id: "12", nome: "Tiras de frango", preço: "R$25,00", ingredientes: "frango empanado com acompanhamento de molhos", image:  require('./assets/image/frangoempanado.png')  ,adicionar:"adicionar +"
+    },
+    { id: "13", nome: "Cebola frita", preço: "R$15,00", ingredientes: "cebolas fritas em rodelas", image:  require('./assets/image/cebolafrita.png'), adicionar:"adicionar+" 
+    },
+]
+
+const renderacompanhamentos = ({ item }: { item: acompanhamentos }) => (
+    <TouchableOpacity style={styles.item}>
+        <Text style={styles.nomeText}>{item.nome}</Text>
+        <Text> ----------------------------------------------------------------------------- </Text>
+        <Text style={styles.preçoText}>{item.preço}</Text>
+        <Text>{item.ingredientes}</Text>   
+        <View style={styles.imgAlign}>  
+        <Image source={item.image} style={styles.image}/>
+        
+
+
+
+        </View>
+    <TouchableOpacity><Text style={styles.adicionar}>{item.adicionar}</Text></TouchableOpacity>
+</TouchableOpacity>
+)
+
+interface Bebidas {
+    id: string;
+    nome: string;
+    preço: string;
+    ingredientes: string;
+    image: any,
+    adicionar:string,
+}
+
+const dados2: Bebidas[] = [
+    {
+    id: "1", nome: "Milkshake de chocolate", preço: "R$10,00", ingredientes: "leite,sorvete de chocolate e calda de chocolate ", image:  require('./assets/image/milkshakechoco.png') ,adicionar:"adicionar +"
+    },
+    {
+    id: "2", nome: "Milkshake de morango", preço: "R$13,00", ingredientes: "leite,sorvete de morango e calda de morango", image:  require('./assets/image/milkshakemorango.png') ,adicionar:"adicionar +"
+    },
+    {
+        id: "3", nome: "Refrigerante", preço: "R$8,00", ingredientes: "Coca-cola, Guaraná e Pepsi ", image:  require('./assets/image/refri.png') ,adicionar:"adicionar +"
+    },
+]
 
 const renderItem = ({ item }: { item: CardapioItem }) => (
     <TouchableOpacity style={styles.item}>
@@ -36,10 +97,34 @@ const renderItem = ({ item }: { item: CardapioItem }) => (
         <Text>{item.ingredientes}</Text>    
         <View style={styles.imgAlign}>  
         <Image source={item.image} style={styles.image}/>
+        
+
+
+
         </View>
-    
+    <TouchableOpacity><Text style={styles.adicionar}>{item.adicionar}</Text></TouchableOpacity>
 </TouchableOpacity>
 )
+
+const renderBebidas = ({ item }: { item: Bebidas }) => (
+    <TouchableOpacity style={styles.item}>
+        <Text style={styles.nomeText}>{item.nome}</Text>
+        <Text> ----------------------------------------------------------------------------- </Text>
+        <Text style={styles.preçoText}>{item.preço}</Text>
+        <Text>{item.ingredientes}</Text>   
+        <View style={styles.imgAlign}>  
+        <Image source={item.image} style={styles.image}/>
+        
+
+
+
+        </View>
+    <TouchableOpacity><Text style={styles.adicionar}>{item.adicionar}</Text></TouchableOpacity>
+</TouchableOpacity>
+)
+
+
+
 function Cardapio(): React.JSX.Element {
     return (
         <View style={styles.container}>
@@ -57,14 +142,30 @@ function Cardapio(): React.JSX.Element {
                 placeholderTextColor={'black'}/> 
         
         </View>
-
+            <ScrollView>
+                
                 <FlatList
                 showsVerticalScrollIndicator={false}
                     data={dados}
                     renderItem={renderItem}
                     keyExtractor={(item) => item.id}
                 />
-
+                <Text style={styles.Texto5}>Porções</Text>
+                <FlatList
+                showsVerticalScrollIndicator={false}
+                    data={dados3}
+                    renderItem={renderacompanhamentos}
+                    keyExtractor={(item) => item.id}
+                />
+                <Text style={styles.Texto5}>Bebidas</Text>
+                <FlatList
+                showsVerticalScrollIndicator={false}
+                    data={dados2}
+                    renderItem={renderBebidas}
+                    keyExtractor={(item) => item.id}
+                />
+                 
+                </ScrollView>
         <View style={styles.footer}>
             <TouchableOpacity>
                 <Image source={require('./assets/image/home.png')} style={styles.footerIcon} />
@@ -85,6 +186,10 @@ function Cardapio(): React.JSX.Element {
                 source={require('./assets/image/pedido.png')} style={styles.footerIcon} />
             </TouchableOpacity>
 
+            <TouchableOpacity style={styles.zap}>
+                <Image 
+                source={require('./assets/image/whats.png')} style={styles.footerIcon} />
+            </TouchableOpacity>
         </View>
         </View>
     );
@@ -138,7 +243,9 @@ const styles = StyleSheet.create({
     header: {
         backgroundColor: '#FF9500',
         alignItems: 'center',
-        paddingVertical: 20
+        paddingVertical: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
 
     },
     headerText: {
@@ -165,11 +272,26 @@ const styles = StyleSheet.create({
     },
     logotipo: {
         width: 180,
-        height: 80
+        height: 100
     },
     imgAlign:{
         flexDirection: 'row',
         alignSelf: 'flex-end'
+    },
+    adicionar:{
+        fontFamily:'arial',
+       marginTop:-200,
+       marginLeft:240,
+       fontSize:15,
+       fontWeight:'bold'
+    },
+    Texto5: {
+        fontWeight: 'bold',
+        fontSize: 35,
+        paddingHorizontal: 125
+    },
+    zap:{
+
     }
     
 });
